1712d8469f3381cfb4338b59d9d5b7b6caf4fef5
Eshilane <eshilane@MB-004718.local>
2020-09-09 18:14:33 -0300

    novo cenário de dados faker

--- features/pages/cadastro_page.rb
+++ features/pages/cadastro_page.rb
@@ -19,15 +19,23 @@ class CadastroPage < SitePrism::Page
     element :address_alias_field, '#alias'
     element :register_btn, '#submitAccount'
 
+    #@ = instância (ao terminar a execução do seu escopo ela deixa de existir)
+    #@@ = classe (no ciclo de vida da classe)
+    #$ = global
+
+
     def iniciar_criacao_conta(email)
-        email_create_account_field.set email
+        puts @email = email.eql?('aleatório') ? Faker::Internet.email(domain: 'guts') : email
+        email_create_account_field.set @email
         create_account_btn.click
     end
 
     def preencher_form_com_dados_fixos
         title_fem_rd.set true
-        first_name_field.set 'Eshilane'
-        last_name_field.set 'Cruz'
+        @@first_name = 'Eshilane'
+        first_name_field.set @@first_name
+        @@last_name = 'Cruz'
+        last_name_field.set @@last_name
         password_field.set '12345'
         day_select.select '18'
         month_select.select 'September'
@@ -43,7 +51,32 @@ class CadastroPage < SitePrism::Page
         address_alias_field.set 'Casa'
     end
 
+    def preencher_form_com_dados_aleatorios
+        title_fem_rd.set true
+        @@first_name = Faker::Name.first_name
+        first_name_field.set @@first_name
+        @@last_name = Faker::Name.last_name
+        last_name_field.set @@last_name
+        password_field.set Faker::Internet.password(min_length: 5, max_length: 10, mix_case: true, special_characters: true)
+        day_select.select '18'
+        month_select.select 'September'
+        year_select.select '1990'
+        newsletter_checkbox.click
+        address_field.set 'Rua A, 15'
+        city_field.set 'Porto Alegre'
+        state_select.click
+        option = state_options.find {|option| option.text.include?("Iowa")}
+        option.click
+        zip_code_field.set '56789'
+        mobile_phone_field.set Faker::PhoneNumber.cell_phone_in_e164
+        address_alias_field.set 'Casa'
+    end
+
     def confirmar_cadastro
         register_btn.click
+    end
+
+    def account_full_name
+        "#{@@first_name} #{@@last_name}"
     end
 end
